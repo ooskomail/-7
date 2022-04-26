61761006fe9ad0b2041c08b97f57fd09076b22ad
komail <komail.gazor@gmail.com>
2022-04-26 15:39:41 +0430

    #7

--- src/index.js
+++ src/index.js
@@ -4,14 +4,26 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const tick = ()=> {
+  const elem = (
+    <div>
+      <h1>oosta</h1>
+   
+    <h2>
+      it is {new Date().toLocaleTimeString()}
+    </h2>
+    </div>
+  )
+  const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ elem
 );
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+setInterval(() => {
+  tick()
+}, 1000);
+
+
+
+
